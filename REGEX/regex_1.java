a4878a27842abee87b99817a1b38a1e43dc66354
Surya <suryask7549@gmail.com>
2025-01-19 23:57:24 +0530

    update regex_1.java

--- REGEX/regex_1.java
+++ REGEX/regex_1.java
@@ -5,7 +5,7 @@ import java.util.regex.*;
 public class regex_1 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("a{3,8}");
-        Matcher matcher = pattern.matcher("aaaaaaaaaa");
+        Matcher matcher = pattern.matcher("aaaaaaaaaaab");
         boolean matchFound = matcher.find();
         System.out.println("Result : " + matchFound);
     }
