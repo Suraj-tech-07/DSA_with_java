9fe2df21a37129b3995d88f8d415b05197c7ec57
Surya <suryask7549@gmail.com>
2025-01-19 23:56:48 +0530

    update regex_1.class and regex_1.java

--- REGEX/regex_1.java
+++ REGEX/regex_1.java
@@ -4,8 +4,8 @@ import java.util.regex.*;
 
 public class regex_1 {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("a{3}");
-        Matcher matcher = pattern.matcher("aaaaa");
+        Pattern pattern = Pattern.compile("a{3,8}");
+        Matcher matcher = pattern.matcher("aaaaaaaaaa");
         boolean matchFound = matcher.find();
         System.out.println("Result : " + matchFound);
     }
