57cc91c25943d4b413f755010fb3b19dc2855286
Surya <suryask7549@gmail.com>
2025-01-20 20:23:55 +0530

    Some other Pattern Checked

--- REGEX/regex_1.java
+++ REGEX/regex_1.java
@@ -1,12 +1,12 @@
-package REGEX;
+// package REGEX;
 
 import java.util.regex.*;
 
 public class regex_1 {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("a{3,8}");
-        Matcher matcher = pattern.matcher("aaaaaaaaaaab");
-        boolean matchFound = matcher.find();
+        Matcher matcher = pattern.matcher("aaaaaaaa");
+        boolean matchFound = matcher.matches();
         System.out.println("Result : " + matchFound);
     }
 }
